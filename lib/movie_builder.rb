8270f87e80785ba4980ecec1ad18d469bde293a5
hukka <kai.kousa@gmail.com>
2008-03-05 10:59:57 +0000

    Changed MovieBuilder to use specific builders for different timeline-formats

--- lib/movie_builder.rb
+++ lib/movie_builder.rb
@@ -7,6 +7,8 @@ require "rexml/document"
 include REXML
 require "model/movie"
 require "model/effect"
+require "channeled_builder"
+require "sequenced_builder"
 require "observable"
 include Observable
 
@@ -24,65 +26,30 @@ class MovieBuilder
     updateAll("Reading the object model from xml(#{xmlFile})...")
     xml = Document.new(File.open(xmlFile))
     
-    name = XPath.first(xml, "//name").text
-    format = XPath.first(xml, "//format").text
-    resolution = XPath.first(xml, "//resolution").text
+    e = XPath.first(xml, "movie")
+    timelineFormat = e.attribute("type").to_s #Get the attribute and convert to string
+    fileVersion = e.attribute("version")
+    
+    #updateAll("Filetype: #{fileType} Version: #{fileVersion}")
+    
+    name = XPath.first(xml, "//metadata/name").text
+    format = XPath.first(xml, "//metadata/format").text
+    resolution = XPath.first(xml, "//metadata/resolution").text
     
     @movie = Movie.new(name, format, resolution)
     
-    #Parse a visual sequence from xml
-    visualSequence = VisualSequence.new
-    visuals = XPath.first(xml, "//visual-sequence")
-    XPath.each(visuals, "visual"){ |e| 
-      type = e.attribute("type")
-      index = e.attribute("index")
-      file = XPath.first(e, "file").text
-      startPoint = XPath.first(e, "start-point").text
-      endPoint = XPath.first(e, "end-point").text
-      place = XPath.first(e, "place").text
-      mute = XPath.first(e, "mute").text
-      volume = XPath.first(e, "volume").text
-      
-      visual = Visual.new(index, type, file, startPoint, endPoint, place, mute, volume)
-      #puts visual.to_str
-      visualSequence.addVideo(visual)      
-    }
-    @movie.visualSequence=(visualSequence)
+    if(timelineFormat == "channeled")
+      updateAll("Building an object model from channeled timeline-format...")
+      channeledBuilder = ChanneledBuilder.new
+      @movie = channeledBuilder.buildMovie(xml, @movie)
+    elsif(timelineFormat == "sequenced")
+      updateAll("Building an object model from sequenced timeline-format...")
+      sequencedBuilder = SequencedBuilder.new
+      @movie = sequencedBuilder.buildMovie(xml, @movie)
+    else
+      updateAll("Unknown timeline-format(#{timelineFormat})")
+    end
     
-    #Parse audio sequence from xml
-    audioSequence = AudioSequence.new
-    audios = XPath.first(xml, "//audio-sequence")
-    XPath.each(audios, "audio"){ |e| 
-      file = XPath.first(e, "file").text
-      startPoint = XPath.first(e, "start-point").text
-      endPoint = XPath.first(e, "end-point").text
-      volume = XPath.first(e, "volume").text
-      offset = XPath.first(e, "offset").text
-      
-      audio = Audio.new(file, volume, startPoint, endPoint, offset)
-      #puts audio.to_str
-      audioSequence.addAudio(audio)      
-    }
-    @movie.audioSequence=(audioSequence)#Add the parsed audiosequence to movie
-    
-    #Parse visual effects from xml
-    effects = []
-    visualEffects = XPath.first(xml, "//visual-effects")
-    XPath.each(visualEffects, "effect"){|e|
-      type = e.attribute("type")
-      properties = {}
-      e.each_element_with_text() { |element| 
-        name = element.attribute("name")
-        value = element.text
-        properties.merge!({ name => value }) #Add a new property to the existing properties
-        #puts "name: #{name} value: #{value}"  
-      }       
-      effect = Effect.new(type, properties)
-      effects << effect #Push the new Effect to effects-array
-    }
-    @movie.effects = effects
-    
-    #visuals.each_element_with_text{|e| puts e} 
     updateAll("...object model finished!")
     return @movie
   end
