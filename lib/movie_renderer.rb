3f3e58719d841a0b47d11afe23dd9e0472c31dda
hukka <kai.kousa@gmail.com>
2008-03-11 14:06:59 +0000

    A stub method that sends an update

--- lib/movie_renderer.rb
+++ lib/movie_renderer.rb
@@ -1,15 +1,18 @@
 # Name: MovieRenderer
 # Author: Kai Kousa
 # Description:
- 
+
+require 'observable'
+include Observable
 
 class MovieRenderer
+  
   def initialize
     
   end
   
   def render(movie)
-    
+    updateAll("MovieRenderer", "Rendering finished!(not really...)")
   end
   
 end
