16a8842f2868912ea3fce37f0da5c5e0cf0007bb
hukka <kai.kousa@gmail.com>
2008-03-12 13:43:06 +0000

    First version of tests for VolumePoint

---
+++ test/model/test_volume_point.rb
@@ -0,0 +1,15 @@
+# Name: TestVolumePoint
+# Author: Kai Kousa
+# Description: Test cases for VolumePoint-class
+require "../lib/model/volume_point"
+require 'test/unit'
+ 
+
+class TestVolumePoint < Test::Unit::TestCase
+  
+  def testInitialize
+    point = VolumePoint.new(100, "00:00:00:00")
+    assert_equal(0, point.point.milliseconds)
+  end
+  
+end
