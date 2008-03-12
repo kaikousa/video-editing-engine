69057773e1afa9e74c1caf3b983657efe6553519
hukka <kai.kousa@gmail.com>
2008-03-12 13:45:27 +0000

    Changed the XML format a bit and made the changes to builder. Volumepoints are now marked as elements instead of element and attribute

--- lib/channeled_builder.rb
+++ lib/channeled_builder.rb
@@ -30,27 +30,29 @@ class ChanneledBuilder
         place = XPath.first(e, "place").text
         mute = XPath.first(e, "mute").text
       
-        volume = XPath.first(e, "volumepoints")
         volumePoints = []
-        volume.each_element_with_text(){|element|
-          point = element.attribute("point")
-          value = element.text
-          volumePoints << VolumePoint.new(value, point)
+        
+        XPath.each(e, "volumepoints/volumepoint"){|volPoint|
+          point = XPath.first(volPoint, "point").text
+          volume = XPath.first(volPoint, "volume").text
+          volumePoints << VolumePoint.new(volume, point)
         }
+        
         #parse effects
         effects = []
         visualEffects = XPath.first(e, "effects")
         visualEffects.each_element_with_text(){|element|
+          puts "effects"
           name = element.attribute("name")
-          startPoint = element.attribute("startPoint")
-          endPoint = element.attribute("endPoint")
+          effectStartPoint = element.attribute("startPoint").to_s
+          effectEndPoint = element.attribute("endPoint").to_s
           parameters = {}
           element.each_element_with_text(){|param|
            paramName = param.attribute("name")
            paramValue = param.text
             parameters.merge!({paramName => paramValue})
           }
-          effect = Effect.new(name, startPoint, endPoint, parameters)
+          effect = Effect.new(name, effectStartPoint, effectEndPoint, parameters)
           effects << effect
         }
       
@@ -70,17 +72,17 @@ class ChanneledBuilder
         file = XPath.first(e, "file").text
         startPoint = XPath.first(e, "start-point").text
         endPoint = XPath.first(e, "end-point").text
-        offset = XPath.first(e, "offset").text
-      
-        volume = XPath.first(e, "volumepoints")
+        place = XPath.first(e, "place").text
+
         volumePoints = []
-        volume.each_element_with_text(){|element|
-          point = element.attribute("point")
-          value = element.text
-          volumePoints << VolumePoint.new(value, point)
+        
+        XPath.each(e, "volumepoints/volumepoint"){|volPoint|
+          point = XPath.first(volPoint, "point").text
+          volume = XPath.first(volPoint, "volume").text
+          volumePoints << VolumePoint.new(volume, point)
         }
       
-        audio = Audio.new(file, startPoint, endPoint, offset, volumePoints)
+        audio = Audio.new(file, startPoint, endPoint, place, volumePoints)
         audioSequence.addAudio(audio)       
       }
       
