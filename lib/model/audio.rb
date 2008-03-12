177156a554f7fb1bf4faef8e89d5291a83adcdf8
hukka <kai.kousa@gmail.com>
2008-03-12 13:44:31 +0000

    Added setter methods and changed 'offset' to 'place', which is how it is marked in the XML.

--- lib/model/audio.rb
+++ lib/model/audio.rb
@@ -5,18 +5,39 @@ require "tools/time_code"
 
 class Audio
   
-  attr_reader(:file, :startPoint, :endPoint, :offset, :volumePoints)
+  attr_reader(:file, :startPoint, :endPoint, :place, :volumePoints)
   
-  def initialize(file, startPoint, endPoint, offset, volumePoints)
+  def initialize(file, startPoint, endPoint, place, volumePoints)
     @file = file
     @startPoint = TimeCode.new(startPoint)
     @endPoint = TimeCode.new(endPoint)
-    @offset = TimeCode.new(offset   )
+    @place = TimeCode.new(place)
     @volumePoints = volumePoints
   end
   
+  def startPoint=(point)
+    @startPoint = point
+  end
+  
+  def endPoint=(point)
+    @endPoint = point
+  end
+  
+  def place=(point)
+    @place = point
+  end
+  
+  def volumePoints=(points)
+    @volumePoints = points
+  end
+  
+  #Returns the length of the clip (endpoint - startpoint)
+  def length()
+    return (@endPoint.milliseconds - @startPoint.milliseconds)
+  end
+  
   def to_str()
-    "File: #{@file} StartPoint: #{@startPoint} EndPoint: #{@endPoint} Volume: #{@volume} Offset: #{@offset}"
+    "File: #{@file} StartPoint: #{@startPoint} EndPoint: #{@endPoint} Volume: #{@volume} Place: #{@place}"
   end
   
 end
