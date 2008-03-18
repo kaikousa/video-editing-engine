214ce71ea028aeef62e2459897f1f9479b41fc7b
hukka <kai.kousa@gmail.com>
2008-03-18 13:18:03 +0000

    Added more tests

--- test/tools/test_time_code.rb
+++ test/tools/test_time_code.rb
@@ -11,4 +11,28 @@ class TestTimeCode < Test::Unit::TestCase
     assert_equal(11060030, time.milliseconds)
   end
   
+  def testPlus
+    a = TimeCode.new("00:00:00:30")
+    b = TimeCode.new("00:00:00:20")
+    assert_equal("0:0:0:50", (a + b).timeCodeStr)
+  end
+  
+  def testPlusBigTime
+    a = TimeCode.new("03:04:20:30")
+    b = TimeCode.new("01:02:10:20")
+    assert_equal("4:6:30:50", (a + b).timeCodeStr)
+  end
+  
+  def testMinus
+    a = TimeCode.new("0:0:0:30")
+    b = TimeCode.new("0:0:0:20")
+    assert_equal("0:0:0:10", (a - b).timeCodeStr)
+  end
+  
+  def testMinusBigTime
+    a = TimeCode.new("03:04:20:30")
+    b = TimeCode.new("01:02:10:20")
+    assert_equal("2:2:10:10", (a - b).timeCodeStr)
+  end
+  
 end
