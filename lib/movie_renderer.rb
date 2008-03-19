08b2d75ea90873f389535edcee88a1b3647aaebb
hukka <kai.kousa@gmail.com>
2008-03-19 11:44:05 +0000

    Implemented basic logic for the rendering process

--- lib/movie_renderer.rb
+++ lib/movie_renderer.rb
@@ -1,18 +1,72 @@
 # Name: MovieRenderer
 # Author: Kai Kousa
-# Description:
+# Description: MovieRenderer is responsible for directing the rendering process.
 
+require 'video_tools'
+require 'audio_tools'
+require 'model/audio_sequence'
 require 'observable'
 include Observable
 
 class MovieRenderer
   
   def initialize
-    
+    @videoTool = VideoTools.new
+    @audioTool = AudioTools.new
   end
   
   def render(movie)
+    updateAll("MovieRenderer", "Processing audio...")
+    audioFile = processAudio(movie)
+    updateAll("MovieRenderer", "...audioprocessing finished!")
+    
+    updateAll("MovieRenderer", "Processing effects...")
+    processEffects(movie)
+    updateAll("MovieRenderer", "...effects finished!")
+    
+    updateAll("MovieRenderer", "Processing visuals...")
+    videoFile = processVisuals(movie)
+    updateAll("MovieRenderer", "...videoprocessing finished!")
+    
+    updateAll("MovieRenderer", "Multiplexing audio and video...")
+    @videoTool.multiplex(videoFile, audioFile, movie.format)
+    updateAll("MovieRenderer", "...multiplexing finished")
+    
     updateAll("MovieRenderer", "Rendering finished!(not really...)")
+  end
+  
+  #Convert -> Trim -> Combine -> Result: Audiotrack
+  def processAudio(movie)
+    audios = movie.audioSequence.sort
+    audios.each{|audio|
+      @audioTool.convertAudio(audio)
+      @audioTool.trimAudio(audio)
+    }
+    audioSequence = AudioSequence.new
+    audioSequence.audios=(audios)
+    movie.audioSequence=(audioSequence)
+    @audioTool.mixAudioSequence(audioSequence)
+  end
+  
+  #Trim/generate -> Combine -> Result: Videotrack
+  def processVisuals(movie)
+    visuals = movie.visualSequence.sort
+    visuals.each {|visual|
+      if(visual.type == "video")
+        @videoTool.trimVideo(movie, visual)
+      elsif(visual.type == "image")
+        @videoTool.createVideoFromImage(movie, visual)
+      elsif(visual.type == "blackness")
+        @videoTool.createVideoFromScratch(movie, visual, "")
+      end
+    }
+    sequence = VisualSequence.new
+    sequence.visuals=(visuals)
+    @videoTool.combineVideo(sequence)
+  end
+  
+  def processEffects(movie)
+    
   end
   
 end
