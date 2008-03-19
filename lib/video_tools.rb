5877c47d929507a7ff2f2cbd1c92d57364190d75
hukka <kai.kousa@gmail.com>
2008-03-19 11:46:27 +0000

    Additional methods. Put some pseudo-code in the comments.

--- lib/video_tools.rb
+++ lib/video_tools.rb
@@ -1,15 +1,26 @@
 # Name: VideoTools
 # Author: Kai Kousa
-# Description:
- 
+# Description: Contains the low-level-methods for videoediting. 
 
 class VideoTools
   def initialize
     
   end
   
-  def trimVideo(videoFile)
+  def trimVideo(visual)
     
+  end
+  
+  def createVideoFromImage(movie, visual)
+    #Create a video file from an image using the properties of Visual-object.
+    #Update type and file of the Visual
+  end
+  
+  def createVideoFromScratch(movie, visual, text)
+    #Render an image and attach a centered text to it.
+    #then create a video file from it
+    #
+    #createVideoFromImage(movie, visual)
   end
   
   def combineVideo(sequence)
