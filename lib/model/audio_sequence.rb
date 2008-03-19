061802aa9bdc2c6447b276a3bae43800b27a2aa9
hukka <kai.kousa@gmail.com>
2008-03-19 11:44:49 +0000

    Optimized sorting(doesn't sort an array that is already sorted)

--- lib/model/audio_sequence.rb
+++ lib/model/audio_sequence.rb
@@ -1,6 +1,6 @@
-# 
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+# Name: AudioSequence
+# Author: Kai Kousa
+# Description: Encapsulates a collection of audios and provides useful methods.
 require 'model/audio'
 
 class AudioSequence
@@ -9,14 +9,26 @@ class AudioSequence
   
   def initialize()
     @audios = []
+    @sorted = false
   end
   
   def addAudio(audio)
     @audios << audio
+    @sorted = false
+  end
+  
+  def audios=(audios)
+    @audios = audios
+    @sorted = false
   end
   
   def sort
-    @audios.sort{|x, y| x.place.milliseconds <=> y.place.milliseconds}
+    #if the sequence hasn't changed since last sort, do not sort again
+    unless @sorted
+      @audios = @audios.sort{|x, y| x.place.milliseconds <=> y.place.milliseconds}
+      @sorted = true
+    end    
+    @audios
   end
 
 end
