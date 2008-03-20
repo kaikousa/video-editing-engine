99c91330885354afeb70106ee7ee7298a71f2f1d
hukka <kai.kousa@gmail.com>
2008-03-20 13:03:02 +0000

    Added console-script. Doesn't need separate "launch"-script anymore

--- lib/vre.rb
+++ lib/vre.rb
@@ -45,3 +45,21 @@ class Vre
     puts "#{sender}: #{message}"
   end
 end
+
+puts "\nVideo Rendering Engine (v.0.1-alpha)"
+puts ""
+
+xmlFile = ARGV[0]
+if(xmlFile == nil)
+  puts "Parameter missing!"
+  puts "USAGE: ruby vre <xmlfile>"
+  puts ""
+else
+  start = Time.now
+
+  vre = Vre.new()
+  vre.main(xmlFile)
+
+  stop = Time.now
+  puts "It took #{stop - start} seconds to finish"
+end
