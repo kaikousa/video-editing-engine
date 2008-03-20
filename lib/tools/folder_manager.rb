c90c6d227f0dee86f94d64bd115d8f1260f8e4bb
hukka <kai.kousa@gmail.com>
2008-03-20 13:02:01 +0000

    Stubs for upcoming features

---
+++ lib/tools/folder_manager.rb
@@ -0,0 +1,10 @@
+# Name: Folder manager
+# Author: Kai Kousa
+# Description: Manages the temporary folders (create, delete...)
+ 
+
+class FolderManager
+  def initialize
+    
+  end
+end
---
+++ lib/tools/svg_exporter.rb
@@ -0,0 +1,9 @@
+# Name: SvgExporter
+# Author: Kai Kousa
+# Description: Creates an SVG-image from a movie. Handy for debugging.
+
+class SvgExporter
+  def initialize
+    
+  end
+end
