7b3199d29912cae62695faa0612403a3224f62b8
hukka <kai.kousa@gmail.com>
2008-03-25 07:26:51 +0000

    Just some commented code

--- lib/audio_tools.rb
+++ lib/audio_tools.rb
@@ -1,27 +1,35 @@
 # Name: AudioTools
 # Author: Kai Kousa
 # Description: Contains low-level-methods for audiohandling and -mixing.
- 
+require 'config'
 
 class AudioTools
   def initialize
-    
+    @settings = Config.instance.settings
   end
   
-  def convertAudio(audio)
+  def convertAudio(audio, project)
     #Convert from original format to .wav
     #Update file-property in the audio
+    #extension = File.extname(video.file)
+    #filename = File.basename(video.file)
+    #filename = filename.split('.')[0] + "_converted.wav"
+    #cmd = @settings['audio_extract'].dup
+    #cmd.sub!('<source>', video.file)
+    #cmd.sub!('<target>', project.converted + "/#{filename}")
+    #system(cmd)
+    #audio.file = project.converted + "/#{filename}"
   end
   
-  def trimAudio(audio)
-    
+  def trimAudio(audio, project)
+    #ecaCmd = 'ecasound -b:256'
   end
   
-  def extractAudio(videoFile)
-    
+  def extractAudio(video, project)
+
   end
   
-  def mixAudioSequence(sequence)
+  def mixAudioSequence(sequence, project)
     
   end
   
--- lib/video_tools.rb
+++ lib/video_tools.rb
@@ -1,14 +1,20 @@
 # Name: VideoTools
 # Author: Kai Kousa
 # Description: Contains the low-level-methods for videoediting. 
+# Contributions: fitVideoToScreen-method was taken from Fooga's original sources.
+
+require 'config'
 
 class VideoTools
   def initialize
-    
+    @settings = Config.instance.settings
   end
   
-  def trimVideo(visual)
-    
+  def trimVideo(movie, visual)
+    #cmd = settings['video_trim']
+    #cmd.sub!('<source>', visual.file)
+    #cmd.sub!('<width>', movie.width)
+    #cmd.sub!
   end
   
   def createVideoFromImage(movie, visual)
@@ -29,6 +35,47 @@ class VideoTools
   
   def multiplex(videoFile, audioFile, format)
     
+  end
+  
+  def fitVideoToScreen ( original_w, original_h, target_w, target_h )
+
+    ratio = original_w.to_f / original_h.to_f
+
+    if ratio > 1.34
+
+      inner_height = ( target_w.to_f / ratio ).round
+
+      padding = target_h.to_f - inner_height
+
+      padding_top = ( padding / 2 ).round
+      padding_bottom = padding_top
+
+      unless padding_top % 2 == 0
+        padding_top = padding_top + 1
+        padding_bottom = padding_bottom - 1
+      end
+
+      if not ( padding_top + padding_bottom + inner_height.round ) == target_h
+
+        height_difference = target_h.to_f - ( padding_top.round + padding_bottom.round + inner_height.round )
+
+        if height_difference > 0
+          inner_height = inner_height.round - height_difference
+        else
+          inner_height = inner_height.round + height_difference
+        end
+
+      end
+
+    else 
+      padding_top = 0
+      padding_bottom = 0
+      inner_width = target_w.to_i
+      inner_height = target_h.to_i
+    end
+
+    output = { "inner_width" => target_w.to_s, "inner_height" => inner_height.round.to_s, "padding_top" => padding_top.round.to_s, "padding_bottom" => padding_bottom.round.to_s }
+
   end
   
 end
