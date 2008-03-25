4c81a1aac1e22b715bb7d0cdd33ec56ab1955aaa
hukka <kai.kousa@gmail.com>
2008-03-25 07:27:38 +0000

    Added comments that should clarify how workspace and lib directories are resolved.

--- lib/config.rb
+++ lib/config.rb
@@ -7,9 +7,9 @@ class Config
     attr_reader :vreRoot, :configPath, :settings
     
     def initialize
-        libPath = File.dirname(File.expand_path(__FILE__)) + "/"
-        @vreRoot = File.expand_path(libPath + "../") + "/"
-        @configPath = @vreRoot + "conf/"
+        libPath = File.dirname(File.expand_path(__FILE__)) + "/" #../VideoRenderingEngine/lib/
+        @vreRoot = File.expand_path(libPath + "../") + "/" #../VideoRenderingEngine/
+        @configPath = @vreRoot + "conf/" #../VideoRenderingEngine/conf/
         @settings = YAML.load_file(@configPath + "settings.yml")
     end
     private :initialize
