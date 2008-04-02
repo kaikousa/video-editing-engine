b08b96cacef12dd8f3f03b8cc7d6b483c6986c31
hukka <kai.kousa@gmail.com>
2008-04-02 07:25:24 +0000

    Added new project - folder that has to be handled. Still quite buggy.

--- lib/tools/folder_manager.rb
+++ lib/tools/folder_manager.rb
@@ -11,32 +11,38 @@ class FolderManager
   
   def createProjectLayout(project)
       workspace = Config.instance.vreRoot + Config.instance.settings['workspace']
-      time = Time.now.strftime("%H%M%S%d%m%Y") #hour minute second day mont year
-      projectFolder = workspace + "/#{project.name.gsub(" ", "_")}-#{time.to_s}"
+      time = Time.now.strftime("%H%M%S%d%m%Y") #hour minute second day month year
+
+      projectFolder = workspace + "/#{project.name}-#{time.to_s}"
       #projectFolder = workspace + "/" + project.name
       project.setProjectFolders(projectFolder)
       createDirectory(project.root)
       createDirectory(project.originals)
       createDirectory(project.converted)
       createDirectory(project.final)
+      createDirectory(project.trimmed)
   end
   
   def cleanProjectFolder(project)
       removeDirectory(project.converted)
       removeDirectory(project.originals)
+      removeDirectory(project.trimmed)
   end
   
   def deleteProjectFolder(project)
       removeDirectory(project.converted)
       removeDirectory(project.originals)
+      removeDirectory(project.trimmed)
       removeDirectory(project.final)
       removeDirectory(project.root)
   end
   
   #Destroys the files in a directory
   def clearDirectory(dir)
+    puts dir
       if(File.exists?(dir))
         Dir.foreach(dir){|entry|
+          puts entry
             if(File.file?(entry))
                 File.delete(entry)
             end
