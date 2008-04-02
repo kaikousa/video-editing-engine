92507800e1f328e2e97f637be983051b818240ac
hukka <kai.kousa@gmail.com>
2008-04-02 07:24:47 +0000

    Removed unnecessary methods. Added implementations to trim- and mix-methods

--- lib/tools/audio_tools.rb
+++ lib/tools/audio_tools.rb
@@ -1,6 +1,8 @@
 # Name: AudioTools
 # Author: Kai Kousa
 # Description: Contains low-level-methods for audiohandling and -mixing.
+# Contributions: Method implementations have been copied and 
+# refactored from work by Matti Venäläinen and Markku Lempinen.
 require 'config'
 
 class AudioTools
@@ -11,30 +13,45 @@ class AudioTools
   def convertAudio(audio, project)
     #Convert from original format to .wav
     #Update file-property in the audio
-    #extension = File.extname(video.file)
-    #filename = File.basename(video.file)
-    #filename = filename.split('.')[0] + "_converted.wav"
-    #cmd = @settings['audio_extract'].dup
-    #cmd.sub!('<source>', video.file)
-    #cmd.sub!('<target>', project.converted + "/#{filename}")
-    #system(cmd)
-    #audio.file = project.converted + "/#{filename}"
+    filename = (File.basename(audio.file)).split(".")[0] + "_converted.wav"
+    cmd = @settings['audio_extract'].dup
+    cmd.sub!('<source>', audio.file)
+    cmd.sub!('<target>', project.converted + "/#{filename}")
+    system(cmd)  
+    audio.file = project.converted + "/#{filename}"
   end
   
   def trimAudio(audio, project)
-    #ecaCmd = 'ecasound -b:256'
-  end
-  
-  def extractAudio(video, project)
-
+    filename = project.converted + "/" + (File.basename(audio.file)).split(".")[0] + "_channel.ewf"
+    
+    ewf = "source = #{audio.file}\n"
+    ewf << "offset = #{audio.place.seconds.to_f}\n"
+    ewf << "start-position = #{audio.startPoint.seconds.to_f}\n"
+    ewf << "length = #{audio.lengthInSeconds.to_f}\n"
+    f = File.new(filename, 'w')
+    f.write(ewf)
+    f.close
+    audio.ewfFile=(filename)
   end
   
   def mixAudioSequence(sequence, project)
+    audios = sequence.audios
+    unless(audios.length == 0)
+      ecaCmd = "ecasound -b:256 "
+      0.upto(audios.length - 1){|i|
+        ecaCmd << " -a:#{i + 1} -i #{audios[i].ewfFile}"
+      }
     
-  end
-  
-  def generateAudioTrack()
+      audiotrackFilename = project.final + "/audiotrack.wav"
     
+      ecaCmd << " -a:all -o #{audiotrackFilename} 1>/dev/null 2>&1"
+      puts ecaCmd
+      system(ecaCmd)
+    else
+      audiotrackFilename = Config.instance.vreRoot + "resources/silencio.wav"
+    end
+    
+    return audiotrackFilename
   end
   
 end
