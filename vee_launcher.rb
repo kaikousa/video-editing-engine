3b2b1a27c6cb0e41905484856a6f7848e96d7fc3
Kai Kousa <kai.kousa@gmail.com>
2009-02-19 21:22:59 +0200

    moved command line code from vee.rb to vee_runner.rb

--- vee_launcher.rb
+++ vee_launcher.rb
@@ -1,2 +1,26 @@
 $: << "lib"
 require "vee"
+#This section is for launching the VRE on the command line.
+
+puts "\nVideo Editing Engine (v.0.2-alpha)"
+puts ""
+
+movie_file = ARGV[0]
+if(movie_file == nil)
+  puts "Parameter missing!"
+  puts "USAGE: ruby vre <file>"
+  puts ""
+  puts "Video Editing Engine supports movies in the following formats:"
+  puts "\t- XML (channeled or sequenced)"
+  puts "\t- JSON"
+  puts "See the documentation for detailed instructions on creating project files"
+  puts ""
+else
+  start = Time.now
+
+  vee = Vee.new()
+  vee.process_movie_file(movie_file)
+
+  stop = Time.now
+  puts "It took #{stop - start} seconds to finish"
+end
